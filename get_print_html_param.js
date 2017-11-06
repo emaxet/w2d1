88edfb52f95a2a8006ac518419bc00e882cd414c
Erik Tammpere <etammpere@gmail.com>
2017-11-06 19:47:21 +0000

    Changed from CLI arguments to a function argument

--- get_print_html_param.js
+++ get_print_html_param.js
@@ -1,11 +1,6 @@
 var https = require('https');
 
-function getAndPrintHTML() {
-
-  var requestOptions = {
-  host: process.argv.slice(2)[0],
-  path: process.argv.slice(2)[1]
-  }
+function getAndPrintHTML(options) {
 
   https.get(requestOptions, function(response) {
       var responseBody = '';
@@ -22,6 +17,12 @@ function getAndPrintHTML() {
 
 }
 
-getAndPrintHTML();
+ var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+
+getAndPrintHTML(requestOptions);
 
 
